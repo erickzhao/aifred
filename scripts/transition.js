445ffe642d4a0bfd37ce80584244cc8fe57a2fbb
erick <erick@hotmail.ca>
2017-05-14 13:55:34 -0400

    Revamp intro page

---
+++ scripts/transition.js
@@ -0,0 +1,7 @@
+'use strict'
+
+let introPage = document.getElementById('#intro-page');
+let navPage = document.getElementById('#nav-page');
+
+introPage.style.visibility = hidden;
+introPage.style.visibility = shown;
