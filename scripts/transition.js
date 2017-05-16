8949f2cb8a41724d0bb96292ab438051139da4d2
erick <erick@hotmail.ca>
2017-05-16 19:12:57 -0400

    Garbage

--- scripts/transition.js
+++ scripts/transition.js
@@ -1,7 +1,20 @@
 'use strict'
 
-let introPage = document.getElementById('#intro-page');
-let navPage = document.getElementById('#nav-page');
+let introPage = document.getElementById('intro-page');
+let navPage = document.getElementById('nav-page');
 
-introPage.style.visibility = hidden;
-introPage.style.visibility = shown;
+introPage.style.visibility = 'visible';
+introPage.style.opacity = 100;
+navPage.style.visibility = 'hidden';
+navPage.style.opacity = 0;
+
+
+function toggleVisibility() {
+
+    let tmp1 = introPage.style.visibility;
+    let tmp2 = introPage.style.opacity;
+    introPage.style.visibility = navPage.style.visibility;
+    introPage.style.opacity = navPage.style.opacity;
+    navPage.style.visibility = tmp1;
+    navPage.style.opacity = tmp2;
+}
