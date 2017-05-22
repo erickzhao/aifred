b7cb20cc428e6f9d9204d5482425ccc3fffd4e89
nykanm <nykan@Nykans-MacBook-Pro-2.local>
2017-05-22 00:42:09 -0400

    removed flatly, fixed logo link

--- flatfy/scripts/link-scroll.js
+++
@@ -1,9 +0,0 @@
-$('a[href^="#"]').on('click', function(event) {
-    var target = $(this.getAttribute('href'));
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top-80
-        }, 1000);
-    }
-});
