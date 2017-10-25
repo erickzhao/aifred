37ad6209a4cab33bcff7dbaa242eacf760bc75c7
nykanm <nykan@Nykans-MacBook-Pro-2.local>
2017-10-25 00:16:19 -0400

    added Eleonore

--- scripts/modal.js
+++ scripts/modal.js
@@ -566,4 +566,27 @@ window.onclick = function(event) {
 
 
 
+var modal22 = document.getElementById('myModal22');
 
+// Get the button that opens the modal
+var btn22 = document.getElementById("myBtn22");
+
+// Get the <span> element that closes the modal
+var span22 = document.getElementsByClassName("close22")[0];
+
+// When the user clicks the button, open the modal 
+btn22.onclick = function() {
+    modal22.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span22.onclick = function() {
+    modal22.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal22) {
+        modal22.style.display = "none";
+    }
+}
